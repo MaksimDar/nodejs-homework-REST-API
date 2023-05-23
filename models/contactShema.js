1433c3dc770b79e5514d26db316cc6a9e1fe518a
Maksym Dovhusha <dovgusha.m@gmail.com>
2023-05-23 17:04:53 +0200

    shema

--- models/contactShema.js
+++ models/contactShema.js
@@ -1,1 +1,19 @@
+const { Shema, model } = require("mongoose");
+const shema = new Shema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
 const
