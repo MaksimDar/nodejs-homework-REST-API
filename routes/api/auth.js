2345721b538fad645c1014e79f17fb9956c037ba
Maksym Dovhusha <dovgusha.m@gmail.com>
2023-05-25 17:29:55 +0200

    1 part

---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const schema = require("../../models/schema");
+
+router.get(
+  "/users.register",
+  (async = (req, res, next) => {
+    console.log("token");
+  })
+);
